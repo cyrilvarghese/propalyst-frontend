f3f828648c22f42e8ed13604ef56846853e92aea
cyrilvarghese <cyrilpdev@gmail.com>
2025-12-20 09:53:19 +0530

    mixp:added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
+import { useEffect } from 'react';
 import "./globals.css";
 import { GoogleTagManager } from "@next/third-parties/google";
-
+import { initMixpanel } from '../lib/mixpanel-client';
 export const metadata: Metadata = {
   title: "Propalyst Property Search",
   description: "Propalyst Property Search",
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
     icon: '/icon.png',
   },
 };
-
+useEffect(() => {
+  initMixpanel(); // Initialize Mixpanel
+}, []);
 export default function RootLayout({
   children,
 }: Readonly<{
